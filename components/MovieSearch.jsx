cecfc4d245737c1f4c29fcc7674c31570caf2b73
Your Name <you@example.com>
2021-01-11 19:52:03 -0600

    initial commit of front end

---
+++ components/MovieSearch.jsx
@@ -0,0 +1,156 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faTrash,
+  faChevronRight,
+  faChevronLeft,
+} from "@fortawesome/free-solid-svg-icons";
+
+export default function MovieSearch() {
+  // Setting up Hooks
+  const [list, setList] = useState(null);
+  const [search, setSearch] = useState("");
+  const [nominees, setNominees] = useState([]);
+  const [banner, setBanner] = useState(false);
+
+  // Search Funtion
+  const handleSearch = async (e) => {
+    setSearch(e.target.value);
+
+    const movieList = await axios.get(
+      `http://www.omdbapi.com/?s=${e.target.value}&apikey=2438c384&type=movie`
+    );
+
+    setList(movieList.data.Search);
+  };
+  // Remove Nominee Function
+  function handleRemoveNominee(e) {
+    const listCopy = [...nominees];
+    const removedNomineeList = listCopy.filter((item) => item !== e);
+    setNominees(removedNomineeList);
+    setBanner(false);
+  }
+  // Nominate Movie Function
+  function handleNomination(e) {
+    if (nominees.length < 5) {
+      const newNomineesList = [...nominees];
+      newNomineesList.push(e);
+      setNominees(newNomineesList);
+      if (newNomineesList.length === 5) {
+        setBanner(true);
+      }
+    }
+  }
+
+  const scroll = (scrollOffset) => {
+    const searchBox = document.getElementById("search-box");
+    searchBox.scrollLeft += scrollOffset;
+  };
+
+  return (
+    <div className="movie-dashboard">
+      {/* Condition Render of Banner and Scroll buttons*/}
+      {banner && (
+        <div className="banner">Maximum of five movies nominated! </div>
+      )}
+      <div className={`scroll ${list ? "showScroll" : ""}`}>
+        <button
+          className={`scrollLeft ${list ? "show" : "hide"}`}
+          onClick={() => scroll(-232)}
+        >
+          <FontAwesomeIcon className="arrow-button" icon={faChevronLeft} />
+        </button>
+
+        <div className="search-results" id="search-box">
+          {list ? (
+            list.map((res) => (
+              <div
+                className={`movie-card ${
+                  !nominees.includes(res.Title) ? "nominate" : "nominated"
+                }`}
+              >
+                <div
+                  className="movie-poster"
+                  style={{
+                    color: "white",
+                    backgroundImage: `url(${
+                      res.Poster == "N/A" ? "/BlankPoster.jpg" : res.Poster
+                    })`,
+                  }}
+                ></div>
+                <div className="movie-info">
+                  <div className="movie-title">
+                    <div className="movie-year">
+                      {res.Title} ({res.Year})
+                    </div>
+                  </div>
+                </div>
+                <div className="movie-buttons">
+                  {!nominees.includes(res.Title) ? (
+                    <button
+                      className="enabled"
+                      onClick={() => {
+                        handleNomination(res.Title);
+                      }}
+                    >
+                      NOMINATE
+                    </button>
+                  ) : (
+                    <button
+                      className="disabled"
+                      onClick={() => handleRemoveNominee(res.Title)}
+                    >
+                      NOMINATED
+                    </button>
+                  )}
+                </div>
+              </div>
+            ))
+          ) : (
+            <div className="welcome">Search for Movies to Nominate</div>
+          )}
+          <div className="dot">.</div>
+        </div>
+        <button
+          className={`scrollRight ${list ? "show" : "hide"}`}
+          onClick={() => scroll(232)}
+        >
+          <FontAwesomeIcon className="arrow-button" icon={faChevronRight} />
+        </button>
+      </div>
+
+      <div className="search-bar">
+        <form class="form">
+          <input
+            type="text"
+            placeholder="Search.."
+            value={search}
+            onChange={handleSearch}
+            name="search"
+          />
+          <i className="fa fa-search" />
+        </form>
+      </div>
+
+      <div className="nominee-list">
+        {nominees.map((res) => (
+          <div className="nominee-card">
+            <div className="nominee-title">{res}</div>
+            <div className="nominee-button">
+              <div
+                className="delete-button"
+                onClick={() => handleRemoveNominee(res)}
+              >
+                <div className="remove">Remove</div>
+                <FontAwesomeIcon className="thrash" icon={faTrash} />
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+
+      <footer>by Tepeu Potter</footer>
+    </div>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,32 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import MovieSearch from "../components/MovieSearch";
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <div className="container">
       <Head>
-        <title>Create Next App</title>
+        <title>The Shoppies by Tepeu Potter</title>
+        <meta name="author" content="Tepeu Potter"></meta>
+        <meta name="description" content="Shopify movie nomination"></meta>
+        <meta
+          name="keywords"
+          content="shopify, shoppies, newest, intern"
+        ></meta>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0"
+        ></meta>
         <link rel="icon" href="/favicon.ico" />
+        <link
+          rel="stylesheet"
+          href="https://use.fontawesome.com/releases/v5.3.1/css/all.css"
+          integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU"
+          crossorigin="anonymous"
+        ></link>
       </Head>
-
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+      <main>
+        <MovieSearch />
       </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
     </div>
-  )
+  );
 }
